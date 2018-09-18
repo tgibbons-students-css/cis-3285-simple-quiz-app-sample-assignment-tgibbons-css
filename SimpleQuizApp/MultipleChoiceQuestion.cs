9bf2870ae18fcf31b70f1d3fe1f9b84edfa0af71
Tom Gibbons <tgibbons@css.edu>
2018-09-18 10:36:10 -0500

    Added support for multiple choice question

---
+++ SimpleQuizApp/MultipleChoiceQuestion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleQuizApplication
+
+{
+    class MultipleChoiceQuestion : Question
+    {
+        String[] possibleAnswers;
+        int answerIndex;
+
+        public MultipleChoiceQuestion(String questionText, String[] possibleAnswers, int answerIndex) : base(questionText)
+        {
+            this.possibleAnswers = possibleAnswers;
+            this.answerIndex = answerIndex;
+        }
+
+        /**
+         * getQuestion()
+         * @return Adds newline after each possible answer
+         */
+
+        public override String getQuestion()
+        {
+            String fullQuestion = questionText + Environment.NewLine;
+            foreach (String s in possibleAnswers)
+            {
+                fullQuestion += s + Environment.NewLine;
+            }
+            return fullQuestion;
+        }
+
+
+        public override String getAnswer()
+        {
+            return possibleAnswers[answerIndex];
+        }
+
+
+        public override bool checkAnswer(String answer)
+        {
+            return (answer.Equals(possibleAnswers[answerIndex], StringComparison.InvariantCultureIgnoreCase));
+        }
+
+    }
+}
